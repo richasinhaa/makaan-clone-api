a20cbd7acb5e3fa27fdbb899ea9395f25b2a6fed
richasinhaa <richa.sinha0603@gmail.com>
2021-12-19 22:24:05 +0800

    Added gitignore

---
+++ src/main/java/com/api/makaanclone/MakaanCloneApplication.java
@@ -0,0 +1,13 @@
+package com.api.makaanclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakaanCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakaanCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/makaanclone/MakaanCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.makaanclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakaanCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
